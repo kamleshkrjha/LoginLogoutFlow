4af0c46766eb82b898111de19c4b84f37cda4177
kamlesh <kamlesh_jha@external.mckinsey.com>
2018-07-18 14:53:48 +0530

    change test user key

--- server/mock-data.js
+++ server/mock-data.js
@@ -2,14 +2,14 @@
   
 const users = {
     "kamlesh": {id:1, username: 'kamlesh', password: '123'},
-    "ramesh": {id:2, username: 'john', password: '123'},
-    "suresh": {id:3, username: 'jane', password: '123'}
+    "john": {id:2, username: 'john', password: '123'},
+    "jane": {id:3, username: 'jane', password: '123'}
 }
 
 const userDetails = {
     "kamlesh": {id:1, username: 'kamlesh', first_name: 'Kamlesh', last_name: 'Jha'},
-    "ramesh": {id:2, username: 'john', first_name: 'John', last_name: 'Doe'},
-    "suresh": {id:3, username: 'jane', first_name: 'Jane', last_name: 'Doe'}
+    "john": {id:2, username: 'john', first_name: 'John', last_name: 'Doe'},
+    "jane": {id:3, username: 'jane', first_name: 'Jane', last_name: 'Doe'}
 }
 
  let _loggedInUsers = [];
